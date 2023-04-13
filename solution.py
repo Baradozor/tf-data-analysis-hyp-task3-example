37bc82762768db2cb15ee6ebb449792e56d89f8f
Владислав Овчинников <81553553+Baradozor@users.noreply.github.com>
2023-04-13 14:52:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+import statsmodels.stats.weightstats as w
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 299666468 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _, pvalue = w.ztest(x, value=500, alternative='larger')
+
+    return pvalue <= 0.07
